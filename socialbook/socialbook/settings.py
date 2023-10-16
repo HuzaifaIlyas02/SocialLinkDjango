57aa98acb132988fac7edea1744da06f45ed23ad
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-16 18:11:45 +0500

    Made Profile Model and user authentication

--- socialbook/socialbook/settings.py
+++ socialbook/socialbook/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
