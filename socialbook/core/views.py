f9f99318ecc274fdb60ec286516b0e67b42df64e
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-17 15:36:16 +0500

    Managed SignUp Procedure

--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to setting page
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user= user_model.id)
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
