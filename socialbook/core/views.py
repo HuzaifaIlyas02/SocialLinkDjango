03cbe9ca46893a596591ec12a742f0ef54ae97cc
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-17 18:08:26 +0500

    managed signin and logout

--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Create your views here
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -43,4 +46,23 @@ def signup(request):
     
 
 def signin(request):
-    return render(request, 'signin.html')
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
