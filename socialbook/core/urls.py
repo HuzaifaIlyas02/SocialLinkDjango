b06de5c879e197b5aaa02c64953a21ea50354b58
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-18 12:09:35 +0500

    added the account setting functionality

--- socialbook/core/urls.py
+++ socialbook/core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    path('settings', views.settings, name='settings'),
 
 ]
--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -11,6 +11,37 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, 'index.html')
 
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
 def signup(request):
 
     if request.method == 'POST':
@@ -31,12 +62,14 @@ def signup(request):
                 user.save()
 
                 #log user in and redirect to setting page
+                user_login = auth.authenticate(username=username,password=password)
+                auth.login(request, user_login)
 
                 #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user= user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
