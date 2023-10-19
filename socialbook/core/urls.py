e2637432149822c5b16d93502dd2de9c0b1a97bd
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-19 06:53:52 +0500

    managed uploading post procedure

--- socialbook/core/urls.py
+++ socialbook/core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
-    path('settings', views.settings, name='settings')
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name = 'upload')
 
 ]
--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -2,21 +2,39 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
+@login_required(login_url='signin')
+def upload(request):
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload') 
+        caption = request.POST['caption']
+
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
 
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
     
     if request.method == 'POST':
+        
         if request.FILES.get('image') == None:
             image = user_profile.profileimg
             bio = request.POST['bio']
