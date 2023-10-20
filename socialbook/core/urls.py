d9be336a53ac6dab1b88197589c380a9522f4e36
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-20 19:30:32 +0500

    Bug Fixed in migration Files and added follow/unfollow functionality

--- socialbook/core/urls.py
+++ socialbook/core/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('upload', views.upload, name = 'upload'),
     path('like-post', views.like_post, name = 'like-post'),
     path('profile/<str:pk>', views.profile, name = 'profile'),
+    path('follow', views.follow, name = 'follow'),
 
 ]
--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 
 # Create your views here
@@ -60,15 +60,27 @@ def profile(request, pk):
     user_profile = Profile.objects.get(user = user_object)
     user_posts = Post.objects.filter(user = pk)
     user_post_length = len(user_posts)
+    
 
-    # print(user_posts.image.url)
+    follwer = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follwer, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
 
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
         'user_post_length': user_post_length,
-
+        'button_text': button_text,
+        'user_followers': user_followers,
+        'user_following': user_following,
     }
     return render(request, 'profile.html', context)
 
@@ -102,6 +114,22 @@ def settings(request):
 
     return render(request, 'setting.html', {'user_profile': user_profile})
 
+@login_required(login_url='signin')
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+    else:
+        return redirect('/')
 
 def signup(request):
 
