2f248126ad15de05918e84014cb1c74c1aa620fb
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-22 12:43:16 +0500

    Added User suggestion to follow

--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 from itertools import chain
+import random
 
 # Create your views here
 
@@ -27,8 +28,34 @@ def index(request):
 
     feed_list = list(chain(*feed))
 
-    posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
+    # user suggestion starts
+
+    all_users = User.objects.all()
+    user_following_all =[]
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:5]})
 
 @login_required(login_url='signin')
 def search(request):
@@ -51,7 +78,7 @@ def search(request):
         
         username_profile_list = list(chain(*username_profile_list))
 
-    return render(request, 'search.html', {'user_profile': user_profile, 'username': username, 'username_profile_list': username_profile_list})
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
 
 @login_required(login_url='signin')
 def upload(request):
