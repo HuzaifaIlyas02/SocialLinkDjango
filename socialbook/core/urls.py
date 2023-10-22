5258ad01da5bf0fd9629c129f4628627fcb6feb8
HuzaifaIlyas02 <huzaifailyas02@outlook.com>
2023-10-22 11:43:24 +0500

    Added search.html, and functionality for searching user

--- socialbook/core/urls.py
+++ socialbook/core/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('like-post', views.like_post, name = 'like-post'),
     path('profile/<str:pk>', views.profile, name = 'profile'),
     path('follow', views.follow, name = 'follow'),
+    path('search', views.search, name = 'search'),
 
 ]
--- socialbook/core/views.py
+++ socialbook/core/views.py
@@ -31,6 +31,29 @@ def index(request):
     return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
 
 @login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains = username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+        
+        username_profile_list = list(chain(*username_profile_list))
+
+    return render(request, 'search.html', {'user_profile': user_profile, 'username': username, 'username_profile_list': username_profile_list})
+
+@login_required(login_url='signin')
 def upload(request):
 
     if request.method == 'POST':
